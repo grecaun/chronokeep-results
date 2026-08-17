a4b8cbf64fd8e3b2fa68af6e9de054e3b99fa36d
James Sentinella <james@sentinella.me>
2026-08-16 17:57:04 -0700

    Changing license from MIT to AGPLv3.

--- types/util.go
+++ types/util.go
@@ -1,3 +1,21 @@
+/*
+Chronokeep Desktop - Race Scoring Software
+Copyright (C) 2026 James Sentinella
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package types
 
 import "regexp"
@@ -7,3 +25,4 @@ var (
 	validYear      = regexp.MustCompile(`^[\-0-9a-zA-Z]+$`).MatchString
 	validEventName = regexp.MustCompile(`^^[A-Za-z'0-9\s/&]+$`).MatchString
 )
+
